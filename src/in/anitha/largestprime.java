e2e8d8cd0956b90a99205abc709dab0b797a4c81
Anitha Jadapalli <anitha.jadapalli@gmail.com>
2013-01-18 22:48:24 +0100

    modified largest prime factor

--- src/in/anitha/largestprime.java
+++ src/in/anitha/largestprime.java
@@ -1,13 +1,23 @@
 package in.anitha;
+/**
+ * Largest PrimeFactor of  600851475143
+ * @author Jadapalli
+ *
+ */
 
 public class largestprime 
 {
+	static long number = 600851475143L;
+	static long highestPrime = -1;
+	/**
+	 * 
+	 * @param args
+	 */
+ 
 	public static void main(String[] args) 
 	{
 
-		long number = 600851475143L;
 
-		long highestPrime = -1;
 		for (long i = 2; i <= number; ++i) 
 		{
 			if (number % i == 0) 
